034556f6c22d032184805febd4d730be692b0d13
Kendahl Ross <kendahlross@SpiderMan.local>
2020-02-20 15:43:15 -0800

    hover and ripple complete

--- site/buttons.js
+++ site/buttons.js
@@ -1,15 +1,15 @@
-$('.simon-button.green').on('click', function() {
-  alert('You clicked the green button');
-});
+// $('.simon-button.green').on('click', function() {
+//   alert('You clicked the green button');
+// });
 
-$('.simon-button.blue').on('click', function() {
-  alert('You clicked the blue button');
-});
+// $('.simon-button.blue').on('click', function() {
+//   alert('You clicked the blue button');
+// });
 
-$('.simon-button.yellow').on('click', function() {
-  alert('You clicked the yellow button');
-});
+// $('.simon-button.yellow').on('click', function() {
+//   alert('You clicked the yellow button');
+// });
 
-$('.simon-button.red').on('click', function() {
-  alert('You clicked the red button');
-});
+// $('.simon-button.red').on('click', function() {
+//   alert('You clicked the red button');
+// });

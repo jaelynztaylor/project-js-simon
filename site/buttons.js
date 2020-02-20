d8299f95af289c94d82de5efdedd4a3f99be186a
jaelynztaylor <jaelynztaylor@users.noreply.github.com>
2020-02-20 15:41:40 -0800

    update

--- site/buttons.js
+++ site/buttons.js
@@ -1,13 +1,14 @@
 $('.simon-button.green').on('click', function() {
-  alert('You clicked the green button');
+  console.log(this);
+  $(this).addClass('waffles');
 });
-
 $('.simon-button.blue').on('click', function() {
   alert('You clicked the blue button');
 });
 
 $('.simon-button.yellow').on('click', function() {
-  alert('You clicked the yellow button');
+  console.log(this);
+  $(this).addClass('waffles');
 });
 
 $('.simon-button.red').on('click', function() {

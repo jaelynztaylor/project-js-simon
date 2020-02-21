bad300869d112d82badbac44a110207cdf8c933d
jaelynztaylor <jaelynztaylor@users.noreply.github.com>
2020-02-21 09:21:30 -0800

    updated buttons.js and main.css

--- site/buttons.js
+++ site/buttons.js
@@ -1,16 +1,14 @@
 $('.simon-button.green').on('click', function() {
-  console.log(this);
-  $(this).addClass('waffles');
+  /*console.log(this);
+  $(this).addClass('waffles');*/
 });
 $('.simon-button.blue').on('click', function() {
-  alert('You clicked the blue button');
 });
 
 $('.simon-button.yellow').on('click', function() {
-  console.log(this);
-  $(this).addClass('waffles');
+  /*console.log(this);
+  $(this).addClass('waffles');*/
 });
 
 $('.simon-button.red').on('click', function() {
-  alert('You clicked the red button');
 });

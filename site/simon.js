22d806d5bfcfae5a13a97ace025ea9d1f7f2b5e3
Jesse Farmer <jesse@20bits.com>
2020-02-24 11:28:19 -0800

    Reorganize simon logic to handle asynchronous user input

--- site/simon.js
+++ site/simon.js
@@ -1,6 +1,5 @@
 let randomColors = ['green', 'red', 'yellow', 'blue'];
 let currentSequence = [];
-let currentSequenceCopy = [];
 let playerSequence = [];
 let currentStep = 0;
 
@@ -21,91 +20,110 @@ function computerMove() {
   currentSequence.push(getRandomColor());
 }
 
-function manipulateBrowser() {
-  console.log('currentSequence length is: ' + currentSequence.length);
-  // if(currentSequence.length === 0){
-  //     return;
-  // }
+function manipulateBrowser(startIndex, onFinish) {
+  console.log(currentSequence);
+  if (currentSequence.length === startIndex -) {
+    onFinish();
+    return;
+  }
 
-  let nextColor = currentSequence.shift();
-  currentSequenceCopy.push(nextColor);
-  console.log('currentSequenceCopy length is: ' + currentSequenceCopy.length);
-  $(`div.simon-button.${nextColor}`).animate({
-    opacity: 0,
-  }), 1000,
-  $(`div.simon-button.${nextColor}`).animate({
-    opacity: 100,
-  });
+  console.log('currentSequence length is: ' + currentSequence.length);
+
+  let nextColor = currentSequence[startIndex];
+
+  $(`div.simon-button.${nextColor}`).animate(
+    {
+      opacity: 0,
+    },
+    {
+      duration: 300,
+      complete: function() {
+        $(this).animate({
+          opacity: 100,
+        }, 300);
+      },
+    },
+  );
+
   setTimeout(() => {
-    manipulateBrowser();
-  },
-  1000);
+    manipulateBrowser(startIndex + 1, onFinish);
+  }, 800);
 }
 
-function startGame() {
+function startRound() {
+  disablePlayerInput();
   computerMove();
-  manipulateBrowser();
-  play(playerSequence);
+  manipulateBrowser(0, function() {
+    enablePlayerInput();
+  });
 }
 
 function endGame() {
+  disablePlayerInput();
   alert('You done fucked up.');
 }
 
 function moveIsCorrect() {
-  if (playerSequence[currentStep - 1] === currentSequenceCopy[currentStep - 1]) {
-    return true;
-  } else if (playerSequence[currentStep - 1] !== currentSequenceCopy[currentStep - 1]) {
-    return false;
-  }
+  return playerSequence[currentStep - 1] === currentSequence[currentStep - 1];
 }
 
 function roundIsDone() {
-  if (currentSequenceCopy[currentSequenceCopy.length - 1] === playerSequence[playerSequence.length - 1]) {
-    return true;
-  }
-  return false;
+  return currentSequence[currentSequence.length - 1] === playerSequence[playerSequence.length - 1];
 }
 
-function playerInput() {
+// let clickHandlers = {
+//   '.simon-button.green': [function() {
+//     playerSequence.push('green');
+//     console.log(`playerSequence is now ${playerSequence}`);
+//   }],
+// };
+
+function enablePlayerInput() {
   $('.simon-button.green').on('click', function() {
     playerSequence.push('green');
+    play(playerSequence);
     console.log(`playerSequence is now ${playerSequence}`);
   });
   $('.simon-button.red').on('click', function() {
     playerSequence.push('red');
+    play(playerSequence);
     console.log(`playerSequence is now ${playerSequence}`);
   });
   $('.simon-button.yellow').on('click', function() {
     playerSequence.push('yellow');
+    play(playerSequence);
     console.log(`playerSequence is now ${playerSequence}`);
   });
+
   $('.simon-button.blue').on('click', function() {
     playerSequence.push('blue');
+    play(playerSequence);
     console.log(`playerSequence is now ${playerSequence}`);
   });
 }
 
+function disablePlayerInput() {
+  $('.simon-button').unbind('click');
+}
+
 function play(playerSequence) {
+  console.log(moveIsCorrect());
+  console.log(currentStep);
+
   if (moveIsCorrect() === true) {
     currentStep += 1;
-    console.log(moveIsCorrect());
-    console.log(currentStep);
   }
 
   if (!moveIsCorrect()) {
+    console.log('Move is not correct');
     endGame();
   }
 
   if (roundIsDone()) {
     currentStep = 0;
-    computerMove();
-    manipulateBrowser();
-    playerInput();
-    play(playerSequence);
-  } else {
-    playerInput();
+    playerSequence = [];
+    startRound();
   }
 }
 
-startGame();
+startRound();
